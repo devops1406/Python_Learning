7e6b027aa6ffa22117bf736cf456b56d863adb6a
Byalihala <CByalihala@ad.harman.com>
2020-01-09 11:52:27 +0530

    Added pwd.py

---
+++ pwd.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+password = input("Enter Passowrd: ")
+
+if name == 'Mary':
+    print('Hello Mary')
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('Wrong password.')
